5e151b5f4bb97f18fe490641711aa876452d757c
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-03-06 12:20:31 +0700

    Refactoring eventListeners Hover Issue Improved
    but not fixed

--- test/pixi-v5-iife/blackhole.js
+++ test/pixi-v5-iife/blackhole.js
@@ -2,7 +2,7 @@
 /// <reference path="node_modules/pixi.js/pixi.js.d.ts" />
 
 var imagePaths = ["../../docs/examples/images/particle.png"];
-PIXI.utils.skipHello(); // remove pixi message in console log
+PIXI.utils.skipHello(); // remove pixi message in console
 
 let enterScreenState = {
 	clicked: false
@@ -110,7 +110,7 @@ var update = function() {
 			y: 0,
 			radius: 170 + radiusTicker,
 			type: 2,
-			minRadius: 150 + radiusTicker
+			minRadius: 150 + radiusTicker / 6
 		};
 		//dick around with expanding particle scale
 
@@ -126,7 +126,6 @@ var update = function() {
 		// 	}
 
 		// if (particle.next === null) {
-		// 	console.log(particle.transform.scale.x);
 		// }
 		// }
 	}
@@ -141,13 +140,11 @@ var update = function() {
 	// emitter.parent.transform.skew.x = Math.sin(delta) / 100;
 };
 
-// Preload the particle images and create PIXI textures from it
-var urls = []; // imagePaths.slice();
-urls.push("../../docs/examples/images/bg.png");
-
+//LOADER IS NOT SETUP CORRECTLY
 //the loader allows pixi to execute actions only when the required utilities are ready
 var loader = PIXI.Loader.shared;
-for (var i = 0; i < urls.length; ++i) loader.add("img" + i, urls[i]);
+// loader.add(imagePaths);
+
 loader.load(function() {
 	//collect the textures, now that they are all loaded
 	var art = imagePaths;
@@ -181,140 +178,134 @@ loader.load(function() {
 	enterText.parent.y = window.innerHeight / 2;
 	emitter.updateOwnerPos(window.innerWidth / 2, window.innerHeight / 2);
 
-	enterText.mouseover = function(mousedata) {
-		console.log("_activeParticlesFirst:", emitter._activeParticlesFirst);
-
-		//-------------------MOUSEOVER: EDIT THE TEXT--------------------
-		this.style.fill = "#FFFFFF";
-		this.style.fontSize = 21;
-		this.text = "- Enter -";
-
-		//-------------------MOUSEOVER: EDIT THE EMITTER------------------
-		if (enterScreenState.clicked === false) {
-			//Creates burst effect
-			emitter.particlesPerWave = 500;
-
-			//Ties into the update function, a smaller number is slower
-			speedController = 0.0003;
-
-			//this is pretty good, problem is needs to ease-in
-			// emitter.startScale.value = 0.5;
-
-			//Editing the radius
-			emitter.spawnCircle = {
-				x: -2,
-				y: 0,
-				radius: 160,
-				type: 2,
-				minRadius: 140
-			};
-
-			//Changes how long each particle survives
-			emitter.minLifetime = 0.82;
-			emitter.maxLifetime = 0.82;
-		}
-
-		/*
-		GOOD SETTINGS:		
-		emitter.particlesPerWave = 500;
-		speedController = 0.0003;
-		emitter.spawnCircle = { x: -2, y: 0, radius: 160, type: 2, minRadius: 140 };	
-		emitter.minLifetime = 0.82;
-		emitter.maxLifetime = 0.82;	
-		*/
-
-		//this one looks quite good: creates a burst effect -- can also do this via particlesPerWave
-		// emitter.frequency = 0.00000000000001;
-	};
-
-	enterText.mouseout = function(mousedata) {
-		if (enterScreenState.clicked === false) {
-			//-------------------MOUSEOUT: RESET THE TEXT---------------------
-			this.style.fill = "silver";
-			this.style.fontSize = 20;
-
-			//-------------------MOUSEOUT: RESET THE EMITTER------------------
-			emitter.particlesPerWave = 1;
-			speedController = 0.001;
-			emitter.spawnCircle = {
-				x: -2,
-				y: 0,
-				radius: 160,
-				type: 2,
-				minRadius: 150
-			};
-			emitter.minLifetime = 2;
-			emitter.maxLifetime = 2;
-		}
-	};
-
-	//CLICK FUNCTION
-	enterText.pointerdown = function() {
-		enterScreenState.clicked = true;
-		//placeholder for spaceCowboy
-		this.text === "Clicked"
-			? (this.text = "- Enter -")
-			: (this.text = "See You Space Cowboy");
-		enterText.interactive = false;
-
-		// edit emitter
-
-		//I SHOULD HAVE A RESET FUNCTION AS I AM USING IT TWICE IN ORDER TO HAVE DRY CODE
-		emitter.minimumSpeedMultiplier = 3;
-		emitter.particlesPerWave = 1;
-		emitter.minLifetime = 3;
-		emitter.maxLifetime = 3;
-		// emitter.spawnCircle = { x: -2, y: 0, radius: 190, type: 2, minRadius: 180 };
-		emitter.minimumScaleMultiplier = 5;
-		// speedController = 0.0015;
-
-		var particle, next;
-		for (particle = emitter._activeParticlesFirst; particle; particle = next) {
-			next = particle.next;
-
-			if (particle.age < 0.6) {
-				particle.age = 0.6;
-			}
-		}
-
-		// 	// emitter.recycle(particle);
-		// 	// if (particle.parent) particle.parent.removeChild(particle);
-		// 	// particle.x = window.innerWidth / 2;
-		// 	// particle.y = window.innerHeight / 2;
-		// 	// particle.rotationSpeed(1000);
-
-		// 	if (particle.next === null) {
-		// 		console.log(particle);
-		// 		console.log(particle.transform.rotation);
-		// 	}
-		// }
-	};
 	// Start the update
 	update();
-	console.log(emitter);
 });
 
-// Upon window resize, resize the canvas to the size of the window
-window.onresize = function() {
-	let _w = window.innerWidth;
-	let _h = window.innerHeight;
-	renderer.resize(_w, _h);
+//MOUSEOVER: Effects from hovering on enterText
+let enterTextHoverEffects = function() {
+	//-------------------MOUSEOVER: EDIT THE TEXT--------------------
+	this.style.fill = "#FFFFFF";
+	this.style.fontSize = 21;
+	this.text = "- Enter -";
 
-	if (!emitter) return;
-	emitter.updateOwnerPos(renderer.screen.width / 2, renderer.screen.height / 2);
-	enterText.parent.x = renderer.screen.width / 2;
-	enterText.parent.y = renderer.screen.height / 2;
+	//-------------------MOUSEOVER: EDIT THE EMITTER------------------
+	if (enterScreenState.clicked === false) {
+		//Creates burst effect
+		emitter.particlesPerWave = 500;
 
+		//Ties into the update function, a smaller number is slower
+		speedController = 0.0003;
+
+		//Editing the radius
+		emitter.spawnCircle = {
+			x: -2,
+			y: 0,
+			radius: 160,
+			type: 2,
+			minRadius: 140
+		};
+
+		//Changes how long each particle survives
+		emitter.minLifetime = 0.6;
+		emitter.maxLifetime = 0.6;
+
+		//this is pretty good, but would want to ease-in
+		// emitter.startScale.value = 0.5;
+	}
+};
+
+//MOUSEOUT: Effects from exiting hover
+let enterTextExitEffects = function() {
+	if (enterScreenState.clicked === false) {
+		//-------------------MOUSEOUT: RESET THE TEXT---------------------
+		this.style.fill = "silver";
+		this.style.fontSize = 20;
+
+		//-------------------MOUSEOUT: RESET THE EMITTER------------------
+		emitter.particlesPerWave = 1;
+		speedController = 0.001;
+		emitter.spawnCircle = {
+			x: -2,
+			y: 0,
+			radius: 160,
+			type: 2,
+			minRadius: 150
+		};
+		emitter.minLifetime = 2;
+		emitter.maxLifetime = 2;
+	}
+};
+
+//MOUSECLICK: Effects from clicking enterText
+let enterTextClickEffects = function() {
+	//update enterScreenState
+	enterScreenState.clicked = true;
+	//placeholder for spaceCowboy
+	this.text === "Clicked"
+		? (this.text = "- Enter -")
+		: (this.text = "See You Space Cowboy");
+	enterText.interactive = false;
+
+	//-------------------MOUSECLICK: EDIT THE EMITTER------------------
+	emitter.minimumSpeedMultiplier = 3;
+	emitter.particlesPerWave = 1;
+	emitter.minLifetime = 3;
+	emitter.maxLifetime = 3;
+	emitter.minimumScaleMultiplier = 5;
+	// speedController = 0.0015;
+
+	//Age some existing particles to shorten new wave spawn
+	var particle, next;
+	for (particle = emitter._activeParticlesFirst; particle; particle = next) {
+		next = particle.next;
+
+		if (particle.age < 0.4) {
+			particle.age = 0.4;
+		}
+	}
+};
+
+enterText.on("mouseover", enterTextHoverEffects);
+enterText.on("mouseout", enterTextExitEffects);
+enterText.on("pointerdown", enterTextClickEffects);
+
+//Kills existing particles upon window resize
+let particleCleanup = function() {
 	for (particle = emitter._activeParticlesFirst; particle; particle = next) {
 		next = particle.next;
 		particle.kill();
 	}
 };
+
+// Cleanly center emitter upon window resize
+window.onresize = function() {
+	//resize the canvas to the size of the window
+	let _w = window.innerWidth;
+	let _h = window.innerHeight;
+	renderer.resize(_w, _h);
+
+	//reposition containers
+	if (!emitter) return;
+	emitter.updateOwnerPos(renderer.screen.width / 2, renderer.screen.height / 2);
+	enterText.parent.x = renderer.screen.width / 2;
+	enterText.parent.y = renderer.screen.height / 2;
+
+	//Kill existing particles
+	particleCleanup();
+};
 window.addEventListener("resize", window.onresize());
 
-//Destroy Emitter Function
+//Destroy Emitter Function -- Placeholder currently
 window.destroyEmitter = function() {
 	emitter.destroy();
 	emitter = null;
 	window.destroyEmitter = null;
 };
+
+//Placeholder printouts
+console.log(
+	emitter,
+	"_activeParticlesFirst:",
+	setTimeout(() => console.log(emitter._activeParticlesFirst), 100)
+);
