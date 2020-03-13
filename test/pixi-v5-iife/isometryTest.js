81a4dcc3032628849dec0bc3edec4745c74392a9
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-03-13 21:21:10 +0700

    Adding atmosphere to planets

--- test/pixi-v5-iife/isometryTest.js
+++ test/pixi-v5-iife/isometryTest.js
@@ -1,14 +1,14 @@
 /*
 Goals:
-1. Get the initial sun setup
-2. Add a corona to the sun -> to be used as atmostphere
-3. Add a planet
-4. Rotate the planet around the sun
-5. Add the planets atmosphere (may differ from the corona setup)
-6. Add shading to the planet
-7. Make the planet stop when hover
+1. Get the initial sun setup DONE 03/13
+2. Add a corona to the sun -> to be used as atmostphere DONE 03/13
+3. Add a planet DONE 03/12
+4. Rotate the planet around the sun DONE 03/12
+5. Add the planets atmosphere (may differ from the corona setup) DONE 03/13
+6. Add shading to the planet -- let's hope this isn't necessary....
+7. Make the planet stop when hover 
 8. Make the planet restart when stopped hovering
-9. Test how adding an orbital line looks
+9. Test how adding an orbital line looks, need to adjust this
 10. Add an info box upon hovering
 11. Create a create planet function 
 12. Add multiple planets
@@ -38,17 +38,17 @@ let _ = undefined;
 const app = new PIXI.Application(rendererOptions);
 
 const isoScalingContainer = new PIXI.Container();
-isoScalingContainer.scale.y = 0.5;
-// isoScalingContainer.scale.x = 0.5;
+isoScalingContainer.scale.y = 0.18;
+isoScalingContainer.scale.x = 0.5;
 isoScalingContainer.position.set(app.screen.width / 2, app.screen.height / 2);
 app.stage.addChild(isoScalingContainer);
 
 const isometryPlane = new PIXI.Graphics();
-isometryPlane.rotation = Math.PI / 4;
+// isometryPlane.rotation = Math.PI / 4;
 isoScalingContainer.addChild(isometryPlane);
 
-let numOfRowCols = 1000;
-isometryPlane.lineStyle(1, 0xffffff); //creates the grid
+let numOfRowCols = 1300;
+isometryPlane.lineStyle(1.2, 0xffffff); //creates the grid
 // for (let i = -numOfRowCols; i <= numOfRowCols; i += 50) {
 // 	isometryPlane.moveTo(-numOfRowCols, i);
 // 	isometryPlane.lineTo(numOfRowCols, i);
@@ -56,8 +56,10 @@ isometryPlane.lineStyle(1, 0xffffff); //creates the grid
 // 	isometryPlane.lineTo(i, numOfRowCols);
 // }
 let startRadius = 100;
-for (let i = 200; i <= numOfRowCols; i += 100) {
+for (let i = 325; i <= numOfRowCols; i += 125) {
 	isometryPlane.drawCircle(0, 0, i);
+	// isometryPlane.drawRoundedRect(0, 0, i, i + 50, 100); //can maybe use this as framing for my window popup on planet hoves
+	// isometryPlane.drawEllipse(0, 0, i, i + 30); //by extending y you can vary the height of a circle with this.
 }
 
 //create the loader
@@ -66,68 +68,58 @@ PIXI.Loader.shared
 	.add("./assets/plutomap1k.jpg")
 	.add("./assets/sunShrunk.jpg")
 	.add("./assets/sun.jpg")
-	.add("./assets/eggHead.png")
 	.load(setup);
 function setup() {
+	//sun color: #cc9f4c
 	let sunTexture =
 		PIXI.Loader.shared.resources["./assets/sunShrunk.jpg"].texture;
-	console.log(sunTexture.width, sunTexture.height);
 	sunTexture.frame = new PIXI.Rectangle(2, 0, 200, 100); //Texture.frame (x, y, width, height)
-	let sunGraphic = new PIXI.Graphics();
-	sunGraphic.lineStyle(0);
-	sunGraphic.beginTextureFill(sunTexture); // can have sunGraphic.beginTextureFill(sunTexture, 0xff00ff, 1); to color the planet
-	sunGraphic.drawCircle(0, 0, 100);
-	sunGraphic.endFill();
-	sunGraphic.setTransform(_, _, _, 1.6, -0.8, _); //setTransform(x, y, x-scale,y-scale,xkew,yskew )
+	let sunGraphic = new PIXI.Graphics()
+		.lineStyle(8, 0xcc9f4c, 0.25, 0.5) //add a semi-transparent corona lineStyle(width, color, alpha,alignment,native),
+		.beginTextureFill(sunTexture)
+		.drawCircle(0, 0, 100)
+		.endFill()
+		.setTransform(_, _, _, 2, _, _); //setTransform(x, y, x-scale,y-scale,xkew,yskew )
 	isometryPlane.addChild(sunGraphic);
+	//add a background sun to create a double layered corona for the sun
+	let backgroundSun = new PIXI.Graphics();
+	backgroundSun
+		.lineStyle(20, 0xcc9f4c, 0.5, 0.5)
+		.drawCircle(0, 0, 102)
+		.setTransform(_, _, _, 2, _, _).filters = [new PIXI.filters.BlurFilter(6)];
+	isometryPlane.addChild(backgroundSun);
 
 	let plutoTexture =
 		PIXI.Loader.shared.resources["./assets/plutomap1k.jpg"].texture;
 	plutoTexture.frame = new PIXI.Rectangle(0, 0, 200, 250); //Texture.frame (x, y, width, height)
-	let plutoGraphic = new PIXI.Graphics();
-	plutoGraphic.lineStyle(0);
-	plutoGraphic.beginTextureFill(plutoTexture); // can have sunGraphic.beginTextureFill(sunTexture, 0xff00ff, 1); to color the planet
-	plutoGraphic.drawCircle(0, 0, 50);
-	plutoGraphic.endFill();
-	plutoGraphic.setTransform(_, _, _, _, _, 0.5);
+	let plutoGraphic = new PIXI.Graphics()
+		.lineStyle(8, 0xc3b6aa, 0.25, 0.5) //add atmostphere
+		.beginTextureFill(plutoTexture)
+		.setTransform(_, _, _, 2, _, _) //setTransform(x, y, x-scale,y-scale,xkew,yskew )
+		.drawCircle(0, 0, 50)
+		.endFill();
 	isometryPlane.addChild(plutoGraphic);
-
-	const sprite4 = new PIXI.projection.Sprite2d(
-		PIXI.Loader.shared.resources["./assets/eggHead.png"].texture
-	);
-	sprite4.proj.affine = PIXI.projection.AFFINE.AXIS_X;
-	sprite4.anchor.set(0.5, 1.0);
-	sprite4.scale.set(0.3, 0.5); // make it small but tall!
-	isometryPlane.addChild(sprite4);
 
 	let textureTicker = 0;
 	let step = 0;
 	app.ticker.add(delta => {
 		textureTicker += 0.7;
-		sunTexture.frame.width = 200 + textureTicker * 2;
-		plutoTexture.frame.width = 200 + textureTicker;
+		step += delta;
 
+		//control scrolling of planet background
+		sunTexture.frame.width = 200 + textureTicker / 3;
+		plutoTexture.frame.width = 200 + textureTicker;
 		sunTexture.updateUvs();
 		plutoTexture.updateUvs();
-
 		sunGraphic.geometry.invalidate();
 		plutoGraphic.geometry.invalidate();
 
-		// sprite3.rotation = step * 0.05;
-		step += delta;
-
-		const radiusPluto = 200;
-		const speedPluto = 0.05;
+		//control movement of a planet
+		const radiusPluto = 450;
+		const speedPluto = 0.02;
 		plutoGraphic.position.set(
 			Math.cos(step * speedPluto * 1) * radiusPluto,
 			Math.sin(step * speedPluto * 1) * radiusPluto
-		);
-
-		const radius4 = 300;
-		const speed4 = 0.01;
-		sprite4.position.set(
-			Math.cos(step * speed4) * radius4,
-			Math.sin(step * speed4) * radius4
 		);
 	});
 }
